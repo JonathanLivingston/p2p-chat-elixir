17f766ab3e8951a900fb8a4080aeb191191198da
Jonathan Livingston <velzedor@yandex.ru>
2026-04-15 02:33:22 +0400

    added location cache to the weather module

--- lib/weather/server.ex
+++ lib/weather/server.ex
@@ -1,6 +1,12 @@
 defmodule Weather do
   use GenServer
+  require Logger
   alias Req
+
+  @poll_interval :timer.seconds(30)
+
+  defstruct location_cache: %{},
+            weather_data: %{}
 
   # Client API
   def start_link(_opts) do
@@ -16,18 +22,36 @@ defmodule Weather do
   end
 
   @impl true
-  def init(state) do
-    {:ok, state}
+  def init(_) do
+    schedule_poll()
+    {:ok, %__MODULE__{}}
+  end
+
+  @impl true
+  def handle_info(:poll_weather, state) do
+    Enum.each(Map.keys(state.weather_data), fn city ->
+      GenServer.cast(self(), {:refresh_async, city})
+    end)
+
+    schedule_poll()
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({_ref, {:ok, %Weather{} = updated_state}}, _) do
+    # Do something with the new data, like updating state
+    {:noreply, updated_state}
   end
 
   @impl true
   def handle_info({_ref, {:ok, data, _status}}, state) do
-    {:noreply, Map.merge(state, %{data.city => data})}
+    {:noreply, update_data(state, data)}
   end
 
   @impl true
   def handle_info({_ref, {:error, error_msg, _status}}, state) do
-    IO.puts("Error fetching weather data: #{error_msg}")
+    Logger.error("Error fetching weather data: #{error_msg}")
     {:noreply, state}
   end
 
@@ -39,11 +63,11 @@ defmodule Weather do
   @impl true
   def handle_cast({:refresh_async, city}, state) do
     Task.Supervisor.async_nolink(Weather.TaskSupervisor, fn ->
-      weather_data = fetch_weather_data(city)
+      fetched_data = fetch_weather_data(city, state)
 
-      case weather_data do
-        %{error: reason} -> {:error, reason, state}
-        data -> {:ok, data, Map.merge(state, %{city => weather_data})}
+      case fetched_data do
+        {cache, weather} -> {:ok, update_data(state, {cache, weather})}
+        %{error: reason} -> {:error, reason}
       end
     end)
 
@@ -52,18 +76,27 @@ defmodule Weather do
 
   @impl true
   def handle_call({:refresh, city}, _from, state) do
-    # Simulate fetching weather data for the city
-    weather_data = fetch_weather_data(city)
-    {:reply, weather_data, Map.merge(state, %{city => weather_data})}
+    data = fetch_weather_data(city, state)
+
+    case data do
+      {cache, weather} -> {:reply, weather, update_data(state, {cache, weather})}
+      %{error: reason} -> {:reply, reason, state}
+    end
   end
 
-  defp fetch_weather_data(city) do
+  defp fetch_weather_data(city, state) do
     cityData =
-      Req.get("https://nominatim.openstreetmap.org/search?city=#{city}&format=json")
-      |> case do
-        {:ok, %{status: 200, body: body}} -> {:ok, body}
-        {:ok, %{status: status}} -> {:error, "API returned #{status}"}
-        {:error, reason} -> {:error, reason}
+      case Map.get(state.location_cache, city) do
+        %{} = cache ->
+          {:ok, [cache]}
+
+        nil ->
+          Req.get("https://nominatim.openstreetmap.org/search?city=#{city}&format=json")
+          |> case do
+            {:ok, %{status: 200, body: body}} -> {:ok, body}
+            {:ok, %{status: status}} -> {:error, "API returned #{status}"}
+            {:error, reason} -> {:error, reason}
+          end
       end
 
     cityWeather =
@@ -80,17 +113,26 @@ defmodule Weather do
 
     case cityWeather do
       {:ok, [%{"current" => current, "current_units" => current_units} | _]} ->
-        %{
-          city: city,
-          temperature: "#{current["temperature"]} #{current_units["temperature"]}",
-          cloud_cover: "#{current["cloud_cover"]} #{current_units["cloud_cover"]}",
-          condition:
-            case round(current["cloud_cover"]) do
-              0 -> "Clear"
-              v when v in 1..50 -> "Partly Cloudy"
-              100 -> "Can't see any sun"
-              _ -> "Cloudy"
-            end
+        {
+          %{
+            city => %{
+              "lat" => elem(cityData, 1) |> hd |> Map.get("lat"),
+              "lon" => elem(cityData, 1) |> hd |> Map.get("lon")
+            }
+          },
+          %{
+            city => %{
+              temperature: "#{current["temperature"]} #{current_units["temperature"]}",
+              cloud_cover: "#{current["cloud_cover"]} #{current_units["cloud_cover"]}",
+              condition:
+                case round(current["cloud_cover"]) do
+                  0 -> "Clear"
+                  v when v in 1..50 -> "Partly Cloudy"
+                  100 -> "Can't see any sun"
+                  _ -> "Cloudy"
+                end
+            }
+          }
         }
 
       {:error, reason} ->
@@ -107,5 +149,19 @@ defmodule Weather do
       {:ok, %{status: status}} -> {:error, "API returned #{status}"}
       {:error, reason} -> {:error, reason}
     end
+  end
+
+  defp schedule_poll do
+    Process.send_after(self(), :poll_weather, @poll_interval)
+  end
+
+  defp update_data(state, updated) do
+    cache = elem(updated, 0)
+    weather = elem(updated, 1)
+
+    state = update_in(state.weather_data, fn current -> Map.merge(current, weather) end)
+    state = update_in(state.location_cache, fn current -> Map.merge(current, cache) end)
+
+    state
   end
 end
